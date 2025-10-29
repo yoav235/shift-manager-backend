2d9db6a0f0eecb01e2b49c57be1928aeff432203
hujiDemocracy <hujidemocracygame@gmail.com>
2025-10-29 09:37:01 +0200

    in getShifts you look with shifts._id instead of shifts.shiftId that doesn't exist

--- src/shifts/ShiftsRoutes.js
+++ src/shifts/ShiftsRoutes.js
@@ -28,7 +28,7 @@ shiftRouter.post('/getShifts', async (req, res) => {
     const shifts = req.body;
     console.log("get shifts for ", shifts._id);
     try {
-        const shiftsDB = await ShiftSchemaModel.find({ shiftId: shifts.shiftId });
+        const shiftsDB = await ShiftSchemaModel.find({ shiftId: shifts._id });
         console.log("All shifts: ", shiftsDB);
         res.status(200).json(successResponseObject("All shifts", shiftsDB));
     } catch (err) {
